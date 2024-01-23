61cb9eeef42c4c614f3b99e5c6b862cb78b3964b
Akshay <156583409+AkshayMele@users.noreply.github.com>
2024-01-24 03:48:38 +0530

    Delete Grade_calculator directory

--- Grade_calculator/build.py
+++
@@ -1,9 +0,0 @@
-from pybuilder.core import use_plugin
-
-use_plugin("python.core")
-use_plugin("python.unittest")
-use_plugin("python.install_dependencies")
-use_plugin("python.distutils")
-
-unittest_test_source = "src/unittest/python"
-default_task = "publish"
